08b2c85ab57a37679ea22d9dd1168eec45a68fde
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-24 23:52:03 +0500

    home component  new useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
 import Menu from "./To-Do-List";
-import Tasks from "./Tasks";
-import Completed from "./Completed";
 
 function App() {
   return (
     <div className="Container">
-      <h1>Jaegar Resto</h1>
       <Menu />
-      <Tasks />
-      <Completed />
     </div>
   );
 }
--- src/To-Do-List/index.jsx
+++ src/To-Do-List/index.jsx
@@ -1,10 +1,19 @@
 import "./Menu.css";
 
+import Tasks from "../Tasks";
+import Completed from "../Completed";
+
 import { useState } from "react";
 import { useEffect } from "react";
 
 function Menu() {
   const [currentDate, setCurrentDate] = useState("");
+
+  const [taskInput, setTaskInput] = useState("");
+
+  function handleAddTask(e) {
+    setTaskInput(e.target.value);
+  }
 
   useEffect(() => {
     // Sanani formatlaymiz va saqlaymiz
@@ -15,20 +24,25 @@ function Menu() {
   return (
     <div className="menu-container">
       <header className="menu-header">
-        <h2 className="menu-header__title">A To-Do-List!</h2>
+        <h1 className="menu-header__title">A To-Do-List!</h1>
         <p className="menu-header__text">
-          "Your a wizzard Harry.. prepare to die" - Hagrid
+          "It is possible to commit no mistakes and still lose. That is not{" "}
+          <br /> a weakness; that is life" - Jean Luc Picard
         </p>
         <p className="menu-header__date">{currentDate}</p>
       </header>
       <nav className="menu-navigation">
         <input
+          value={taskInput}
+          onChange={handleAddTask}
           type="text"
           placeholder="What's your tasks today?"
           className="menu-navigation__input"
         />
         <button className="menu-navigation__button">+ Add Item</button>
       </nav>
+      <Completed />
+      <Tasks />
     </div>
   );
 }
